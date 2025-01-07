335a35a59bbd775e18cfca3036b99b8073d063e8
heuzef <contact@heuzef.com>
2025-01-07 16:15:29 +0100

    Prepare data script

--- src/prepare_data.py
+++ src/prepare_data.py
@@ -1,7 +1,33 @@
-# Créez un script Python prepare_data.py dans le dossier src qui permet de 
-# charger les données, de les nettoyer et de les diviser en un jeu d'entraînement et un jeu de test. 
-# Vous pourrez utiliser les librairies pandas et scikit-learn pour cela.
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 
-# La variable cible dans le dataset initial est Chance of Admit, n'hésitez pas à supprimer les variables que vous jugez de manière logique inutiles pour votre modélisation.
+# Load Data
+df = pd.read_csv("../data/raw/admission.csv")
 
-# Suite à cela, vous devez obtenir les fichiers suivants X_test, X_train, y_test, y_train et les sauvegarder dans le dossier data/processed. 
+# Remove the "Serial No."
+df = df.drop(columns=["Serial No."])
+
+# Renaming
+df = df.rename(columns={
+    "GRE Score": "GRE_Score",
+    "TOEFL Score": "TOEFL_Score",
+    "University Rating": "University_Rating",
+    "SOP": "SOP",
+    "LOR ": "LOR",
+    "CGPA": "CGPA",
+    "Research": "Research",
+    "Chance of Admit ": "Chance_of_Admit"
+})
+
+# Set features and target
+X, y = df.drop(["Chance_of_Admit"], axis = 1), df["Chance_of_Admit"]
+
+# Split data
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Export processed data
+X_train.to_csv("../data/processed/X_train.csv", index=False)
+X_test.to_csv("../data/processed/X_test.csv", index=False)
+y_train.to_csv("../data/processed/y_train.csv", index=False)
+y_test.to_csv("../data/processed/y_test.csv", index=False)

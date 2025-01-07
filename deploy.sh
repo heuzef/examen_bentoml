0287c90cce8b4735ee02c334470d71827bf7503f
heuzef <contact@heuzef.com>
2025-01-07 16:13:53 +0100

    Delete deploy.sh

--- deploy.sh
+++
@@ -1,5 +0,0 @@
-bentoml serve service:examen_bentoml --reload
-bentoml build
-bentoml containerize examen_bentoml:latest
-docker run -p 3000:3000 examen_bentoml:latest
-docker save -o bento_image.tar examen_bentoml

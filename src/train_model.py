cc72930ed7f02848ae57484b065a2b70251c9154
heuzef <contact@heuzef.com>
2025-01-07 16:46:50 +0100

    Train model script

--- src/train_model.py
+++ src/train_model.py
@@ -1,7 +1,32 @@
-# Créez un script Python train_model.py dans le dossier src qui permet de charger les données d'entraînement, de créer un modèle de régression et de l'entraîner.
+import numpy as np
+import pandas as pd
+from sklearn.linear_model import LinearRegression, Ridge, Lasso
+from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error
+import bentoml
+import joblib
 
-# Après avoir entrainé votre modèle sur les données d'entraînement, testez et affichez ses performances sur les données de test. 
-# Vous pourrez utiliser la métrique de votre choix (R2, RMSE, MAE, ...).
+# Load data
+X_train = pd.read_csv('data/processed/X_train.csv')
+X_test = pd.read_csv('data/processed/X_test.csv')
+y_train = pd.read_csv('data/processed/y_train.csv')
+y_test = pd.read_csv('data/processed/y_test.csv')
 
-# Enfin si les performances de votre modèle sont satisfaisantes suite aux tests successifs que vous aurez faits, vous devez sauvegarder votre modèle dans le Model Store de BentoML.
-# Verifier avec 'bentoml list'
+# Train/Pred
+model = Lasso(alpha=0.1)
+model.fit(X_train, y_train)
+y_pred = model.predict(X_test)
+
+# Eval
+mse = mean_squared_error(y_test, y_pred)
+rmse = mean_squared_error(y_test, y_pred, squared=False)
+mae = mean_absolute_error(y_test, y_pred)
+r2 = r2_score(y_test, y_pred)
+
+print(f'MSE: {mse}')
+print(f'RMSE: {rmse}')
+print(f'MAE: {mae}')
+print(f'R2: {r2}')
+
+# Save to BentoML Model Store and folder
+bentoml.sklearn.save_model("admissions_model", model)
+joblib.dump(model, 'models/admissions_model.joblib')
